a6cf04964a9a298e0cbcbeb98bbc75a4f10d12c2
therealcalle <cally.x666@googlemail.com>
2026-03-03 12:50:52 +0100

    Delete wgControl directory

--- wgControl/backend/state.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from .hue import HueBridge
-from .sse import SSEManager
-
-# Shared application state
-bridge = HueBridge()
-sse_manager = SSEManager()
-event_task: asyncio.Task | None = None
